91883d060bd2f0e1ae5a37f25a1c91bdb0fc452d
Magic Man <65839659+Magician357@users.noreply.github.com>
2023-11-17 09:40:23 -0800

    text box

--- main.js
+++ main.js
@@ -18,6 +18,8 @@ function moveto(start,end,duration,x){
     return start+((end-start)*smoothStep(x/duration));
 }
 
+var random_x=window.innerWidth*Math.random();
+
 function get_left(x){
     if (between(0,x,500)){
         return 10;
@@ -27,22 +29,31 @@ function get_left(x){
         return 260;
     } else if (between(2000,x,3000)){
         return moveto(260,500,1000,x-2000);
-    } else {
+    } else if (between(3000,x,5000)) {
         return 500;
+    } else {
+        return random_x
     }
 }
 
 function get_top(x){
     if (between(0,x,500)){
         return moveto(0,window.innerHeight/2-100,500,x);
-    } else {
+    } else if (between(500,x,4000)) {
         return (window.innerHeight/2)-100;
+    } else if (between(4000,x,5000)) {
+        return moveto((window.innerHeight/2)-100, window.innerHeight*2, 900, x-4000);
+    } else {
+        return window.innerHeight+2500-(x/2)
     }
 }
 
 const redbox=document.getElementById("redbox");
 const greenbox=document.getElementById("greenbox");
 const bluebox=document.getElementById("bluebox");
+
+const testText=document.getElementById("text-wall-test");
+console.log(testText);
 
 const scroll_indicator=document.getElementById("scroll_indicator");
 
@@ -53,7 +64,7 @@ addEventListener("scroll", (event) => {
 
     scroll_indicator.innerText=Math.round(scrollAmount*100)/100;
 
-    if (scrollAmount>=0&&scrollAmount<=1000){
+    if ((scrollAmount>=0&&scrollAmount<=1000)||scrollAmount>=5000){
         redbox.classList.remove("hidden");
     } else {
         redbox.classList.add("hidden");
@@ -69,11 +80,14 @@ addEventListener("scroll", (event) => {
     bluebox.style.left=get_left(scrollAmount)+"px";
     bluebox.style.top=get_top(scrollAmount)+"px";
 
-    if (scrollAmount>=2000){
+    if (scrollAmount>=2000&&scrollAmount<=5000){
         greenbox.classList.remove("hidden");
     } else {
         greenbox.classList.add("hidden");
     }
     greenbox.style.left=get_left(scrollAmount)+"px";
     greenbox.style.top=get_top(scrollAmount)+"px";
+
+    testText.style.top=`${5000.0-scrollAmount}px`;
+    // console.log(testText.style.transform);
 });
